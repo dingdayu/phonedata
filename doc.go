f43db7df5646873d51df6e6c21e5480ad66fcbcf
dingdayu <614422099@qq.com>
2019-08-01 22:55:03 +0800

    update doc.go
    Signed-off-by: dingdayu <614422099@qq.com>

--- doc.go
+++ doc.go
@@ -1,17 +1,12 @@
-/**
-Package phonedata 内地手机号归属信息.
-
-Data file from: https://github.com/ls0f/phone
-
-data file format:
-	  | 4 bytes |                  <- phone.dat 版本号
-	  ------------
-	  | 4 bytes |                  <-  第一个索引的偏移
-	  -----------------------
-	  |  offset - 8         |      <-  记录区
-	  -----------------------
-	  |  index              |      <-  索引区
-	  -----------------------
-
-*/
+//Package phonedata 内地手机号归属信息.
+// data file from: https://github.com/ls0f/phone
+// data file format:
+//	  | 4 bytes |                  <- phone.dat 版本号
+//	  ------------
+//	  | 4 bytes |                  <-  第一个索引的偏移
+//	  -----------------------
+//	  |  offset - 8         |      <-  记录区
+//	  -----------------------
+//	  |  index              |      <-  索引区
+//	  -----------------------
 package phonedata
